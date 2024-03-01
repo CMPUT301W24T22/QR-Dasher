cbe6ad34d39227ac17e8351c940109fac40be354
Jaspreet Singh <115124290+jaz404@users.noreply.github.com>
2024-02-29 22:43:45 -0700

    Linking firebase to the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,9 @@ android {
 dependencies {
     implementation("com.google.zxing:core:3.4.1")
 
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,11 +6,16 @@ import static android.graphics.Color.WHITE;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -28,6 +33,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+// zxing lib
+import com.google.firebase.FirebaseApp;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -36,11 +43,28 @@ import com.google.zxing.qrcode.QRCodeWriter;
 
 import org.w3c.dom.Text;
 
+// adding firebase references
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
     ImageView qrimage;
     Button generateQR;
-
     EditText inputText;
+
+    // Firebase link
+    private FirebaseFirestore db;
+    private CollectionReference generatedQRCodes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +72,13 @@ public class MainActivity extends AppCompatActivity {
         qrimage = findViewById(R.id.qrCode); // image
         generateQR = findViewById(R.id.generateQR); // button
         inputText = findViewById(R.id.inputText); // TextView
+
+        // Create Collection in Firebase
+        FirebaseApp.initializeApp(this);
+
+        db = FirebaseFirestore.getInstance();
+
+        generatedQRCodes = db.collection("generatedQRCodes");
 
         generateQR.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +88,24 @@ public class MainActivity extends AppCompatActivity {
                     String text = inputText.getText().toString();
                     Bitmap qrCode = createBitmap(text);
                     qrimage.setImageBitmap(qrCode);
+
+                    String qrCodeString = convertQRtoString(qrCode);
+                    generatedQRCodes.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot querySnapshots,
+                                            @Nullable FirebaseFirestoreException error) {
+                            if (error != null) {
+                                Log.e("Firestore", error.toString());
+                                return;
+                            }
+                            if (querySnapshots != null) {
+                                AddQRtoFirebase(qrCodeString,text);
+
+                            }
+                        }
+                    });
+
+
                 }
             }
         });
@@ -83,6 +132,41 @@ public class MainActivity extends AppCompatActivity {
         Bitmap myBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
         myBitmap.setPixels(pixels, 0, width,0,0,width,height);
         return myBitmap;
+    }
+
+    private String convertQRtoString(Bitmap qrCode){
+        // Create a new output Stream (base64 string)
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // compress the qr code
+        qrCode.compress(Bitmap.CompressFormat.PNG, 100,baos);
+        byte[] imageBytes = baos.toByteArray();
+        // return the String in base64 format
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
+    }
+
+    private void AddQRtoFirebase(String qrCodeString, String eventID){
+        // eventID will be the ID linked with the QR code/ Content of the QR code
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //Create a Hashmap to store the QR code string
+        Map<String, Object> qrCodeData = new HashMap<>();
+        qrCodeData.put("image", qrCodeString);
+
+        db.collection("generatedQRCodes")
+                .document(eventID)
+                .set(qrCodeData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("QR", "Successfully uploaded to firestore");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("QR", "Couldn't be uploaded to firestore");
+                        e.printStackTrace();
+                    }
+                });
     }
 
 }

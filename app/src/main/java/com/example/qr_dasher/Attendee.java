e15860f768e21ff01a3edd50572081ecd65d66fe
saipranav1512 <145928964+saipranav1512@users.noreply.github.com>
2024-03-07 00:29:09 -0700

    Update Attendee.java

--- app/src/main/java/com/example/qr_dasher/Attendee.java
+++ app/src/main/java/com/example/qr_dasher/Attendee.java
@@ -1,14 +1,15 @@
 package com.example.qr_dasher;
 
-import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Attendee extends AppCompatActivity {
+public class AttendeeDashboard extends AppCompatActivity {
     private Button notificationButton, editProfileButton, qrCodeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,7 @@ public class Attendee extends AppCompatActivity {
             public void onClick(View v) {
                 // Handle attendee role selection
                 // For example, start a new activity for attendee tasks
-                Intent intent = new Intent(Attendee.this, Notifications.class);
+                Intent intent = new Intent(AttendeeDashboard.this, Notifications.class);
                 startActivity(intent);
             }
         });
@@ -34,7 +35,7 @@ public class Attendee extends AppCompatActivity {
             public void onClick(View v) {
                 // Handle attendee role selection
                 // For example, start a new activity for attendee tasks
-//                Intent intent = new Intent(Attendee.this, Attendee.class);
+//                Intent intent = new Intent(AttendeeDashboard.this, AttendeeDashboard.class);
 //                startActivity(intent);
             }
         });
@@ -44,11 +45,27 @@ public class Attendee extends AppCompatActivity {
             public void onClick(View v) {
                 // Handle attendee role selection
                 // For example, start a new activity for attendee tasks
-                Intent intent = new Intent(Attendee.this, ScanQR.class);
-                startActivity(intent);
+                Intent intent = new Intent(AttendeeDashboard.this, ScanQR.class);
+                startActivityForResult(intent, 1);
             }
         });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) { // Check if the result is from ScanQR activity
+            if (resultCode == RESULT_OK) {
+                String scannedText = data.getStringExtra("scannedText");
+                Toast.makeText(this, "Scanning successful " + scannedText, Toast.LENGTH_SHORT).show();
+                // Implement firestore check
+            } else {
+                // Handle case where scanning was canceled or failed
+                Toast.makeText(this, "Scanning failed or canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }

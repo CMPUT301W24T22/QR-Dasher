35d6480b86e730f319e53fdd74a0eb01994b64c3
rithwikkb <111675689+rithwikkb@users.noreply.github.com>
2024-03-08 15:12:35 -0700

    Update SendNotification.java

--- app/src/main/java/com/example/qr_dasher/SendNotification.java
+++ app/src/main/java/com/example/qr_dasher/SendNotification.java
@@ -5,12 +5,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+/**
+ * SendNotification activity allows users to compose and send notifications.
+ * It provides input fields for notification title and content, along with a button to send the notification.
+ */
 public class SendNotification extends AppCompatActivity {
     private EditText notificationTitleEditText;
     private EditText notificationContentEditText;
     private Button sendNotificationButton;
-
+    /**
+     * Initializes the activity's UI components and sets up click listeners.
+     *
+     * @param savedInstanceState This activity's previously saved state, or null if it has no saved state.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

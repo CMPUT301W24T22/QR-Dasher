5cc2d93f93cc62a52ec294148cf2092f710e8ba4
rithwikkb <111675689+rithwikkb@users.noreply.github.com>
2024-03-08 10:08:44 -0700

    Update reuseQRcodes.java

--- app/src/main/java/com/example/qr_dasher/reuseQRcodes.java
+++ app/src/main/java/com/example/qr_dasher/reuseQRcodes.java
@@ -1,5 +1,6 @@
 package com.example.qr_dasher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
